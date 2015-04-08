f69dee718bd892cf62c267e9d90646ede8f9d6ed
Hlynur <hlynurinn@gmail.com>
2015-04-08 00:08:13 +0000

    db connection production string path changed

--- server/config/config.js
+++ server/config/config.js
@@ -8,7 +8,7 @@ module.exports = {
 		port: process.env.PORT || 3030
 	},
 	production: {
-		db: 'mongodb://hlynurinn:hlynurErMed7henduR@ds037611.mongolab.com:37611/hlynurinn',
+		db: 'mongodb://hlynurinn:FloatTitan1C.@ds061741.mongolab.com:61741/notemakerdb',
 		rootPath: rootPath,
 		port: process.env.PORT || 80
 	}
